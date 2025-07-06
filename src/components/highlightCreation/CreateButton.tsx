8ee58ff3848ce8805b2715505c07722396a56868
Coleman Lai <colemancflai@yahoo.com>
2025-07-06 00:13:18 -0700

    Added button css (that took way too long)

--- src/components/highlightCreation/CreateButton.tsx
+++ src/components/highlightCreation/CreateButton.tsx
@@ -4,9 +4,9 @@ import { CreateButtonProps } from '@/types/types';
 // Floating button to create a new highlight
 export default function CreateButton({ onClick }: CreateButtonProps): JSX.Element {
     return (
-        <button onClick={onClick}>
-            <span>Create</span>
-            <span>+</span>
+        <button onClick={onClick} className='create-button'>
+            <span className='create-text'>Create</span>
+            <span className='create-icon'>+</span>
         </button>
     );
 }
